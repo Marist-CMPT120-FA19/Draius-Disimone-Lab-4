e6914cb8b9c26617cff2c4a622bc329e79cdf84e
Draius <41041066+draius@users.noreply.github.com>
2019-09-24 15:58:56 -0400

    Code

--- trafficlight.py
+++ trafficlight.py
@@ -1,1 +1,30 @@
+#traffic light draius disimone 
+from graphics import*
 
+def main():
+#open new window to do 
+	win= GraphWin()
+	#define shape
+	shape= Rectangle(Point(2,20), Point(40,100))
+	shape.setOutline("black")
+	shape.draw(win)
+	shape.setFill("black")
+	#red light
+	red=Circle(Point(20,40),8)
+	red.setOutline("black")
+	red.draw(win)
+	red.setFill("red")
+	#yellow light
+	yellow=Circle(Point(20,60), 8)
+	yellow.setOutline("black")
+	yellow.setFill("yellow")
+	yellow.draw(win)
+	#green light
+	green=Circle(Point(20,80),8)
+	green.setOutline("black")
+	green.setFill("green")
+	green.draw(win)
+	#click to close window
+	win.getMouse()
+	win.close()
+main()
